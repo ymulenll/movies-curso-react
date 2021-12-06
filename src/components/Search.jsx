b220c555794763d76902c97e2772fb7f8e98adab
ymulenll <ymulenll@gmail.com>
2021-12-06 01:27:19 -0300

    update to react router v6

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,10 @@
 import styles from "./Search.module.css";
 import { FaSearch } from "react-icons/fa";
-import { useHistory } from "react-router";
-import { useQuery } from "../hooks/useQuery";
+import { useSearchParams } from "react-router-dom";
 
 export function Search() {
-  const query = useQuery();
+  const [query, setQuery] = useSearchParams();
   const search = query.get("search");
-
-  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,7 +21,9 @@ export function Search() {
           aria-label="Search Movies"
           onChange={(e) => {
             const value = e.target.value;
-            history.push("/?search=" + value);
+
+            setQuery({ search: value });
+            // navigate("/?search=" + value);
           }}
         />
         <FaSearch size={20} color="black" className={styles.searchButton} />
